b288c5a07ba30a4071730d0e3e2daa297f831bbd
pelleservan <97622610+pelleservan@users.noreply.github.com>
2022-03-28 16:08:34 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="header.css">
+	<title>Les Potes à La Compotes</title>
+</head>
+
+<body>
+	<div>
+		<div id="band">
+			<h1>Les Potes à La Compotes</h1>
+		</div>
+		<div id="menu">
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li class="menu-deroulant">
+     					<a href="#">Les lieux</a>
+     					<ul class="sous-menu">
+     					   <li><a href="rawcountries.php">BXL</a></li>
+     					   <li><a href="rawdepartments.php">Grand Moulin</a></li>
+     					   <li><a href="rawemployees.php">Jaures</a></li>
+     					   <li><a href="rawjobs.php">Hotel de ville</a></li>
+    					</ul>
+   					</li>
+				</ul>
+			</nav>
+		</div>
+	</div>
+</body>
+</html>  
